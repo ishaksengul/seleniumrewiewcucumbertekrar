0507c48eea8175680d28f741861240a1c39842f3
ishaksengul <67099229+ishaksengul@users.noreply.github.com>
2021-11-30 11:46:48 +0100

    Update GoogleRunner.java

--- src/test/java/runners/GoogleRunner.java
+++ src/test/java/runners/GoogleRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "stepdefinitions",
-        tags = "@datatable1",
+        tags = "@googleArama",
         dryRun =false
 )
 
